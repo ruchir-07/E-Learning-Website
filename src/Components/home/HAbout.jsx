9f97fb47a86cba45f8ef98960ab8b01da7fc58af
ruchir-07 <99608006+ruchir-07@users.noreply.github.com>
2022-12-25 23:03:39 +0530

    testimonials section done

--- src/Components/home/HAbout.jsx
+++ src/Components/home/HAbout.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { coursesCard } from '../../dummydata'
+import OnlineCourses from '../allcourses/OnlineCourses'
 import Title from '../common/title/Title'
 
 const HAbout = () => {
@@ -59,6 +60,7 @@ const HAbout = () => {
             </div>
             </div>
         </section>
+        <OnlineCourses />
     </>
   )
 }
--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AboutCard from '../about/AboutCard'
 import HAbout from './HAbout'
 import Hero from './hero/Hero'
+import Test from './testimonial/Test'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Hero />
         <AboutCard />
         <HAbout />
+        <Test />
     </>
   )
 }
